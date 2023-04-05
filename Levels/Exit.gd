80c876bcf90b33536590d0f905e2552fa6153f58
JackNewt <122556891+JackNewt@users.noreply.github.com>
2023-04-05 10:00:35 -0400

    Fixed Level warp

--- Levels/Exit.gd
+++ Levels/Exit.gd
@@ -5,5 +5,5 @@ func _on_Exit_to_2_body_entered(body):
 	if body.name == "Player":
 		if name == "Exit_to_2":
 			var _target = get_tree().change_scene("res://Levels/Level2.tscn")
-		if name == "Exit_to_2":
+		if name == "Exit_to_Menu":
 			var _target = get_tree().change_scene("res://Menu.tscn")
